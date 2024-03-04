7ad434519d7f21f83d18833762a0bea51dae23c2
Marcelo Gomes <marcelo.gomj@gmail.com>
2024-03-04 14:36:09 -0300

    add auth middleware

---
+++ Server/middlewares/use-authentification.ts
@@ -0,0 +1,37 @@
+import { preHandlerHookHandler } from "fastify"
+import { verify } from "jsonwebtoken";
+import prisma from "../services/prisma";
+import { JwtFormatDecoded } from "../types/validation";
+import { verifyAsync } from "../services/jwt";
+
+const useAuthentification: preHandlerHookHandler = async (req, res, done) => {
+  const jwtDecoded = await verifyAsync(req.headers?.authorization ?? "") as JwtFormatDecoded;
+
+  if (!jwtDecoded) {
+    res.send("JWT invalid")
+    return;
+  }
+
+  const { username } = jwtDecoded;
+
+  const user_profile = await prisma.user.findUnique({
+    select: {
+      id: true,
+      username: true,
+      role: true
+    },
+    where: {
+      username
+    }
+  })
+
+  req.params = {
+    ...req.params,
+    user_profile
+  };
+
+  done()
+}
+
+
+export default useAuthentification;
